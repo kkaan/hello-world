954b2b02bbaf4c076f44af2e59df2b31160bf281
kkaan <kankean@gmail.com>
2016-09-27 09:26:45 +1000

    successful coding of rockpaperscissors

--- rockpaperscissors/rockpaperscissors.py
+++ rockpaperscissors/rockpaperscissors.py
@@ -26,12 +26,12 @@ def start():
     
 def game():
     player = move()
-    computer = randint(1,3)
+    computer = random.randint(1,3)
     result(player, computer)
     return play_again()
 
 def move():
-    while true:
+    while True:
         print
         player = raw_input("Rock = 1\nPaper = 2\nScissors = 3. Make your move: ")
         try:
@@ -42,21 +42,40 @@ def move():
             pass
         print "I didn't understand that try again nincompoop"
             
-def result():
+def result(player, computer):
     print "1..."
     time.sleep(1)
     print "2..."
     time.sleep(1)
     print "3..."
     time.sleep(0.5)
-    print "computer threw {0}".format(names[computer])
+    print "Computer threw {0}".format(names[computer])
     global player_score, computer_score
     if player == computer:
         print "tie game"
-        
-    
-    
-    
+    else:
+        if rules[player] == computer:
+            print "your victory has been assured"
+            player_score += 1
+        else:
+            print "the computer laughs as you realise you have been defeated"
+            computer_score += 1
+            
+def play_again():
+    answer = raw_input("Want to play again? y/n: ")
+    if answer in ("y", "Y", "YES", "yes", "Yes"):
+        return answer
+    else:
+        print "thanks, see you again later"
+
+def scores():
+    global player_score, computer_score
+    print "HIGH SCORES"
+    print "Player:", player_score
+    print "Computer:", computer_score
+
+if __name__ == "__main__":
+    start()
     
     
 
